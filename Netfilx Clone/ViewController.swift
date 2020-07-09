483610335307eea793be0183e4b3fcdd4b7eac66
idev <insert@kakao.com>
2020-07-09 23:39:28 +0900

    Initial Commit

---
+++ Netfilx Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netfilx Clone
+//
+//  Created by iDev0 on 2020/07/09.
+//  Copyright © 2020 Ju Young Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
